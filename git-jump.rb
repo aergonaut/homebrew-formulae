1202512916c51257a7a10be2dc93241b0cd68cb7
Chris Fung <aergonaut@gmail.com>
2015-03-10 13:02:48 -0700

    git-jump v1.0.1

--- git-jump.rb
+++ git-jump.rb
@@ -1,7 +1,7 @@
 class GitJump < Formula
-  url "https://github.com/aergonaut/git-jump/archive/v1.0.tar.gz"
+  url "https://github.com/aergonaut/git-jump/archive/v1.0.1.tar.gz"
   homepage "https://github.com/aergonaut/git-jump"
-  sha256 "d99e7bf35b1a19f523fbca64e8576999b4f2560c5e533b50493916106aeb8f89"
+  sha256 "e5d177b631f45e58a1977dea7a9f905fbb4321122e9df9cab9df1aeffb21dcbe"
 
   depends_on "git"
   depends_on "thoughtbot/formulae/pick"
